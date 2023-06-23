b750e7b026a885e02dc0c97cc8996207f6d9e07e
Usman Siddiqui <siddiquiusman328@gmail.com>
2023-06-23 11:37:17 +0500

    ...

--- public/frontend/src/app.jsx
+++ public/frontend/src/app.jsx
@@ -1,1 +1,14 @@
 import React from 'react';
+import {createRoot} from 'react-dom/client'
+import Navbar from './navbar';
+
+
+const App = () => {
+    return (
+        <Navbar/>
+    )
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App/>)
