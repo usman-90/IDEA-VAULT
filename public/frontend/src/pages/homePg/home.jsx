4784964f1e17a302a67fb612ee9457b7da33ef34
Usman Siddiqui <siddiquiusman328@gmail.com>
2023-07-08 02:29:28 +0500

    ...

---
+++ public/frontend/src/pages/homePg/home.jsx
@@ -0,0 +1,17 @@
+import Showcase from "../../components/bgVideo/showcase";
+import Heading from "../../components/headin/heading";
+import Category from "./idea'scategory";
+import FaqSection from "../../components/faq/faq";
+const Home = () => {
+  return (
+    <>
+      <Showcase />
+      <Heading text="Idea's Categories" />
+      <Category />
+      <Heading text="Frequently Ask Questions" />
+      <FaqSection />
+    </>
+  );
+};
+
+export default Home;
