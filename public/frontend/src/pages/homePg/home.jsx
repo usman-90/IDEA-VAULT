403ade7505466295464fb0a74fdb8fcaf6b4e7ae
Usman Siddiqui <siddiquiusman328@gmail.com>
2023-07-08 12:46:29 +0500

    hehe

--- public/frontend/src/pages/homePg/home.jsx
+++ public/frontend/src/pages/homePg/home.jsx
@@ -2,12 +2,15 @@ import Showcase from "../../components/bgVideo/showcase";
 import Heading from "../../components/headin/heading";
 import Category from "./idea'scategory";
 import FaqSection from "../../components/faq/faq";
+import React from "react";
+
 const Home = () => {
   return (
     <>
       <Showcase />
       <Heading text="Idea's Categories" />
       <Category />
+
       <Heading text="Frequently Ask Questions" />
       <FaqSection />
     </>
