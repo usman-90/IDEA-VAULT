857c3b68caea9c74f0d60c59758da8ec95ca9344
Rajja Farhan <rajjafarhan2@gmail.com>
2023-07-18 19:35:35 +0500

    thisisme

--- public/backend/src/routes/unprotectedRoutes.ts
+++ public/backend/src/routes/unprotectedRoutes.ts
@@ -24,11 +24,9 @@ unprotectedRouter.get("/getideaupdates/:ideaid", getIdeaUpdates);
 unprotectedRouter.get("/getideabycategory/:id", getIdeaByCategory);
 unprotectedRouter.get("/getcommentsandreplies/:id", getCommentsAndReplies);
 unprotectedRouter.get("/getvotes/:ideaid", getVotes);
-<<<<<<< HEAD
 unprotectedRouter.get("/getoneuser/:userid",getOneUser)
 // unprotectedRouter.get("/getcategory",getIdeaByCategory)
 
-=======
 unprotectedRouter.get("/getoneuser/:userid", getOneUser);
 unprotectedRouter.get("/getuserinfo/:userid", getUserInfo);
 unprotectedRouter.get("/getideasbyuserid/:userid", getIdeaByUserId);
@@ -36,6 +34,5 @@ unprotectedRouter.get(
   "/checkspecificvisiblity/:userid/:ideaid",
   checkSpecificVsiblity
 );
->>>>>>> 8ab18d7e611aeead6675b1504bedd21895d589b4
 
 export default unprotectedRouter;
